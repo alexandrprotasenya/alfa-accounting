5c220cc7d3f1bffbcdc2fc175ecefc9f3bf6bd3b
Alexander Protasenya <protasenya.alexandr@gmail.com>
2025-03-12 15:34:38 +0300

    disable auth

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
-if not st.experimental_user.is_logged_in:
-    if st.button("Log in with Google"):
-        st.login()
-    st.stop()
+# if not st.experimental_user.is_logged_in:
+#     if st.button("Log in with Google"):
+#         st.login()
+#     st.stop()
 
-if st.button("Log out"):
-    st.logout()
+# if st.button("Log out"):
+#     st.logout()
 
-st.markdown(f"Welcome! {st.experimental_user.name}")
+# st.markdown(f"Welcome! {st.experimental_user.name}")
 
 pg = st.navigation(
     [
